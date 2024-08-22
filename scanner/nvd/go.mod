f12a3b118a80271be31bfdcb1a9957bfc0b1bebd
Victor Dorneanu <victor.dorneanu@sap.com>
2024-08-22 11:47:06 +0200

    Remove e2e tests for now

--- scanner/nvd/go.mod
+++ scanner/nvd/go.mod
@@ -12,11 +12,14 @@ require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/alexflint/go-arg v1.4.2 // indirect
 	github.com/alexflint/go-scalar v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/pprof v0.0.0-20240727154555-813a5fbdbec8 // indirect
 	github.com/matryer/is v1.4.1 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.5.11 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
@@ -26,6 +29,8 @@ require (
 	golang.org/x/sys v0.23.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- scanner/nvd/test/e2e/processor_test.go
+++ scanner/nvd/test/e2e/processor_test.go
@@ -3,89 +3,89 @@
 
 package e2e_test
 
-import (
-	"encoding/json"
-	"fmt"
+// import (
+// 	"encoding/json"
+// 	"fmt"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"github.wdf.sap.corp/cc/heureka/scanner/nvd/models"
-)
+// 	. "github.com/onsi/ginkgo/v2"
+// 	. "github.com/onsi/gomega"
+// 	"github.wdf.sap.corp/cc/heureka/scanner/nvd/models"
+// )
 
-var _ = Describe("Submitting Issues", Ordered, Label("e2e", "Issues", "create"), func() {
-	When("no issues exist", Label("CreateIssue"), func() {
-		Context("and the CVE is valid", func() {
-			It("creates a new Issue", func() {
-				newCve := models.Cve{}
-				cveJson := `
-				{
-					"id": "CVE-2023-0001",
-					"descriptions": [
-						{
-							"lang": "en",
-							"value": "Sample description of the vulnerability in English."
-						},
-						{
-							"lang": "es",
-							"value": "Descripción de ejemplo de la vulnerabilidad en español."
-						}
-					]
-				}
-				`
-				err := json.Unmarshal([]byte(cveJson), &newCve)
-				Expect(err).To(BeNil())
+// var _ = Describe("Submitting Issues", Ordered, Label("e2e", "Issues", "create"), func() {
+// 	When("no issues exist", Label("CreateIssue"), func() {
+// 		Context("and the CVE is valid", func() {
+// 			It("creates a new Issue", func() {
+// 				newCve := models.Cve{}
+// 				cveJson := `
+// 				{
+// 					"id": "CVE-2023-0001",
+// 					"descriptions": [
+// 						{
+// 							"lang": "en",
+// 							"value": "Sample description of the vulnerability in English."
+// 						},
+// 						{
+// 							"lang": "es",
+// 							"value": "Descripción de ejemplo de la vulnerabilidad en español."
+// 						}
+// 					]
+// 				}
+// 				`
+// 				err := json.Unmarshal([]byte(cveJson), &newCve)
+// 				Expect(err).To(BeNil())
 
-				// Create new Issue
-				_, err = cveProcessor.CreateIssue(&newCve)
-				Expect(err).To(BeNil())
-			})
-		})
-	})
+// 				// Create new Issue
+// 				_, err = cveProcessor.CreateIssue(&newCve)
+// 				Expect(err).To(BeNil())
+// 			})
+// 		})
+// 	})
 
-	When("issues exist", Label("GetIssueId"), func() {
-		Context("and the CVE has no metrics", func() {
-			It("returns a valid issue_id", Label("CVE:NoMetrics"), func() {
-				cve := models.Cve{}
-				cveJson := `
-				{
-					"id": "CVE-2024-20083",
-					"descriptions": [
-						{
-							"lang": "en",
-							"value": "Sample description of the vulnerability in English."
-						},
-						{
-							"lang": "es",
-							"value": "Descripción de ejemplo de la vulnerabilidad en español."
-						}
-					],
-					"metrics": {}
-				}
-				`
-				err := json.Unmarshal([]byte(cveJson), &cve)
-				Expect(err).To(BeNil())
+// 	When("issues exist", Label("GetIssueId"), func() {
+// 		Context("and the CVE has no metrics", func() {
+// 			It("returns a valid issue_id", Label("CVE:NoMetrics"), func() {
+// 				cve := models.Cve{}
+// 				cveJson := `
+// 				{
+// 					"id": "CVE-2024-20083",
+// 					"descriptions": [
+// 						{
+// 							"lang": "en",
+// 							"value": "Sample description of the vulnerability in English."
+// 						},
+// 						{
+// 							"lang": "es",
+// 							"value": "Descripción de ejemplo de la vulnerabilidad en español."
+// 						}
+// 					],
+// 					"metrics": {}
+// 				}
+// 				`
+// 				err := json.Unmarshal([]byte(cveJson), &cve)
+// 				Expect(err).To(BeNil())
 
-				fmt.Printf("reponame: %s", cveProcessor.IssueRepositoryName)
+// 				fmt.Printf("reponame: %s", cveProcessor.IssueRepositoryName)
 
-				// Get IssueId
-				issueId, err := cveProcessor.GetIssueId(&cve)
-				Expect(err).To(BeNil())
+// 				// Get IssueId
+// 				issueId, err := cveProcessor.GetIssueId(&cve)
+// 				Expect(err).To(BeNil())
 
-				// Get IssueRepositoryId
-				issueRepositoryId, err := cveProcessor.GetIssueRepositoryId()
-				Expect(err).To(BeNil())
+// 				// Get IssueRepositoryId
+// 				issueRepositoryId, err := cveProcessor.GetIssueRepositoryId()
+// 				Expect(err).To(BeNil())
 
-				fmt.Printf("Severity: %s\n",cve.SeverityVector())
+// 				fmt.Printf("Severity: %s\n",cve.SeverityVector())
 
-				// Create new IssueVariant
-				issueVariantId, err := cveProcessor.CreateIssueVariant(
-					issueId,
-					issueRepositoryId,
-					&cve,
-				)
-				Expect(err).To(BeNil())
-				fmt.Printf("Issue ID: %s\tIssue Variant ID: %s\n", issueId, issueVariantId)
-			})
-		})
-	})
-})
+// 				// Create new IssueVariant
+// 				issueVariantId, err := cveProcessor.CreateIssueVariant(
+// 					issueId,
+// 					issueRepositoryId,
+// 					&cve,
+// 				)
+// 				Expect(err).To(BeNil())
+// 				fmt.Printf("Issue ID: %s\tIssue Variant ID: %s\n", issueId, issueVariantId)
+// 			})
+// 		})
+// 	})
+// })
