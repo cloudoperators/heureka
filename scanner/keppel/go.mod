946402ce556ebdd1e861579493a08918370476b3
Michael Reimsbach <MR201190@gmail.com>
2024-08-22 16:36:24 +0200

    feat(scanner): add keppel scanner (#151)
    * feat(scanner): init keppel scanner

--- scanner/keppel/go.mod
+++ scanner/keppel/go.mod
@@ -1,3 +1,35 @@
 module github.com/cloudoperators/heureka/scanners/keppel
 
 go 1.22.6
+
+require (
+	github.com/aquasecurity/trivy v0.54.1
+	github.com/gophercloud/gophercloud v1.14.0
+	github.com/gophercloud/gophercloud/v2 v2.1.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/pkg/errors v0.9.1
+)
+
+require (
+	github.com/Khan/genqlient v0.7.0 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/alexflint/go-arg v1.4.2 // indirect
+	github.com/alexflint/go-scalar v1.0.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.11 // indirect
+	golang.org/x/mod v0.19.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/tools v0.23.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
+
+require (
+	github.com/aquasecurity/trivy-db v0.0.0-20240718084044-d23a6ca8ba04 // indirect
+	github.com/google/go-containerregistry v0.20.1 // indirect
+	github.com/machinebox/graphql v0.2.2
+	github.com/package-url/packageurl-go v0.1.3 // indirect
+	github.com/samber/lo v1.46.0 // indirect
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+)
---
+++ scanner/keppel/processor/processor.go
@@ -0,0 +1,183 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package processor
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/cloudoperators/heureka/scanners/keppel/client"
+	"github.com/cloudoperators/heureka/scanners/keppel/models"
+	log "github.com/sirupsen/logrus"
+)
+
+type Processor struct {
+	Client *graphql.Client
+}
+
+func NewProcessor(cfg Config) *Processor {
+	httpClient := http.Client{}
+	gClient := graphql.NewClient(cfg.HeurekaUrl, &httpClient)
+	return &Processor{
+		Client: &gClient,
+	}
+}
+
+func (p *Processor) ProcessRepository(repository models.Repository) (*client.Component, error) {
+	r, err := client.CreateComponent(context.Background(), *p.Client, &client.ComponentInput{
+		Name: repository.Name,
+		Type: client.ComponentTypeValuesContainerimage,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	component := r.GetCreateComponent()
+
+	log.WithFields(log.Fields{
+		"componentId": component.Id,
+		"component":   component,
+	}).Info("Component created")
+
+	return component, nil
+}
+
+func (p *Processor) ProcessManifest(manifest models.Manifest, componentId string) (*client.ComponentVersion, error) {
+	r, err := client.CreateComponentVersion(context.Background(), *p.Client, &client.ComponentVersionInput{
+		Version:     manifest.Digest,
+		ComponentId: componentId,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	componentVersion := r.GetCreateComponentVersion()
+
+	log.WithFields(log.Fields{
+		"componentVersionId": componentVersion.Id,
+		"componentVersion":   componentVersion,
+	}).Info("ComponentVersion created")
+
+	return componentVersion, nil
+}
+
+func (p *Processor) ProcessReport(report models.TrivyReport, componentVersionId string) {
+	for _, result := range report.Results {
+		for _, vulnerability := range result.Vulnerabilities {
+			issue, err := p.GetIssue(vulnerability.VulnerabilityID)
+			if err != nil {
+				log.WithFields(log.Fields{
+					"vulnerabilityID":    vulnerability.VulnerabilityID,
+					"issueID":            issue.Id,
+					"issuePrimaryName":   issue.PrimaryName,
+					"componentVersionID": componentVersionId,
+					"report":             report.ArtifactName,
+				}).WithError(err).Error("Error while getting issue")
+				continue
+			}
+			if issue == nil {
+				log.WithFields(log.Fields{
+					"vulnerabilityID": vulnerability.VulnerabilityID,
+				}).Warning("Issue not found")
+				continue
+				// use this for inserting issues, necessary to test without nvd scanner
+				// i, err := p.CreateIssue(vulnerability.VulnerabilityID, vulnerability.Description)
+				// if err != nil {
+				// 	fmt.Println(err)
+				// 	continue
+				// }
+				// issue = &i
+			}
+			_, err = client.AddComponentVersionToIssue(context.Background(), *p.Client, issue.Id, componentVersionId)
+
+			if err != nil {
+				log.WithFields(log.Fields{
+					"issueId":            issue.Id,
+					"componentVersionId": componentVersionId,
+				}).WithError(err).Error("Could not add component version to issue")
+			} else {
+				log.WithFields(log.Fields{
+					"issueId":            issue.Id,
+					"componentVersionId": componentVersionId,
+				}).Info("Added issue to componentVersion")
+			}
+		}
+
+	}
+}
+
+func (p *Processor) GetComponent(name string) (*client.Component, error) {
+	r, err := client.ListComponents(context.Background(), *p.Client, &client.ComponentFilter{
+		ComponentName: []string{name},
+	}, 1)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var component *client.Component
+	if len(r.Components.Edges) > 0 {
+		component = r.Components.Edges[0].GetNode()
+	}
+
+	return component, nil
+}
+
+func (p *Processor) GetComponentVersion(version string) (*client.ComponentVersion, error) {
+	r, err := client.ListComponentVersions(context.Background(), *p.Client, &client.ComponentVersionFilter{
+		Version: []string{version},
+	}, 1)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var componentVersion *client.ComponentVersion
+	if len(r.ComponentVersions.Edges) > 0 {
+		componentVersion = r.ComponentVersions.Edges[0].GetNode()
+	}
+
+	return componentVersion, nil
+}
+
+func (p *Processor) GetIssue(primaryName string) (*client.Issue, error) {
+	r, err := client.ListIssues(context.Background(), *p.Client, &client.IssueFilter{
+		PrimaryName: []string{primaryName},
+	}, 1)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var issue *client.Issue
+	if len(r.Issues.Edges) > 0 {
+		issue = r.Issues.Edges[0].GetNode()
+	}
+
+	return issue, nil
+}
+
+func (p *Processor) CreateIssue(primaryName string, description string) (*client.Issue, error) {
+	r, err := client.CreateIssue(context.Background(), *p.Client, &client.IssueInput{
+		PrimaryName: primaryName,
+		Description: description,
+		Type:        "Vulnerability",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	issue := r.GetCreateIssue()
+
+	log.WithFields(log.Fields{
+		"issueId": issue.Id,
+		"issue":   issue,
+	}).Info("Issue created")
+
+	return issue, nil
+}
