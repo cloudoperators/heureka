2063bd64974c422cd7050ebdc0b53eb1750d99ec
Michael Reimsbach <mr201190@gmail.com>
2024-08-23 14:54:08 +0200

    wip

---
+++ scanner/k8s-assets/processor/processor.go
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package processor
+
+import (
+	"net/http"
+
+	"github.com/Khan/genqlient/graphql"
+)
+
+type Processor struct {
+	Client *graphql.Client
+}
+
+func NewProcessor(cfg Config) *Processor {
+	httpClient := http.Client{}
+	gClient := graphql.NewClient(cfg.HeurekaUrl, &httpClient)
+	return &Processor{
+		Client: &gClient,
+	}
+}
+
+func (p *Processor) ProcessPort() {
+
+}
