40417d066249db8af4d0b3364aa0f1b3e1ccb489
Victor Dorneanu <victor.dorneanu@sap.com>
2024-09-03 12:36:51 +0200

    feat(scanner): Implement K8s scanner (#184)
    * Implement K8s scanner
    
    * Automatic application of license header
    
    * Clean up
    
    * Signing previous commits
    
    * Sign with SSH key
    
    * Sign with SSH key (again)
    
    * Sign with SSH key (again testing)
    
    * Implement K8s scanner
    
    * Automatic application of license header
    
    * Clean up
    
    * Signing previous commits
    
    * Sign with SSH key
    
    * Sign with SSH key (again)
    
    * Sign with SSH key (again testing)
    
    * Signing previous commits
    
    * Sign with SSH key
    
    * Sign with SSH key (again)
    
    * Sign with SSH key (again testing)
    
    * Signing previous commits
    
    * Sign with SSH key
    
    * Sign with SSH key (again)
    
    * Sign with SSH key (again testing)
    
    * WIP
    
    * Restore go.mod
    
    * Wip
    
    * Add tests
    
    * Automatic application of license header
    
    * Wip
    
    * Improve logic
    
    * Factorize KubeConfig
    
    * Fixes #discussion_r1740426730
    
    * Fixes https://github.com/cloudoperators/heureka/pull/184#discussion_r1733993763
    
    ---------
    
    Co-authored-by: License Bot <license_bot@github.com>
    Co-authored-by: David Rochow <david.rochow@sap.com>

---
+++ scanner/k8s-assets/scanner/config.go
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package scanner
+
+type Config struct {
+	KubeConfigPath    string `envconfig:"KUBE_CONFIG_PATH" default:"~/.kube/config" required:"true" json:"-"`
+	KubeconfigContext string `envconfig:"KUBE_CONFIG_CONTEXT"`
+	KubeconfigType    string `envconfig:"KUBE_CONFIG_TYPE" default:"oidc"`
+	SupportGroupLabel string `envconfig:"SUPPORT_GROUP_LABEL" default:"ccloud/support-group" required:"true"`
+	ServiceNameLabel  string `envconfig:"SERVICE_NAME_LABEL" default:"ccloud/service" required:"true"`
+	ScannerTimeout    string `envconfig:"SCANNER_TIMEOUT" default:"30"`
+}
