679b51f70b266ba663a531064a92f005c370014b
Michal Krzyz <michalkrzyz@gmail.com>
2024-09-04 12:55:28 +0200

    feat(authN): Implement Token Based Authentication (#83)
    Review changes - token generator should have configurable expiration time

--- tools/token_generator/main.go
+++ tools/token_generator/main.go
@@ -3,23 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+const defaultExpiresInHours = 30 * 24
 
 type TokenClaims struct {
 	Version string `json:"version"`
 	jwt.RegisteredClaims
 }
 
-func GenerateJWT() (string, error) {
+func GenerateJWT(jwtSecret []byte, expireIn time.Duration) (string, error) {
 	claims := TokenClaims{
 		Version: "0.3.1",
 		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expireIn)),
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 			Issuer:    "heureka",
 			Subject:   "testUser",
@@ -36,11 +37,23 @@ func GenerateJWT() (string, error) {
 }
 
 func main() {
-	if os.Getenv("JWT_SECRET") == "" {
+	var jwtSecret []byte
+	jwtSecretVar := os.Getenv("JWT_SECRET")
+	if jwtSecretVar == "" {
 		panic("JWT_SECRET environment variable not set")
+	} else {
+		jwtSecret = []byte(jwtSecretVar)
 	}
 
-	token, err := GenerateJWT()
+	var expiration time.Duration
+	expiresInHours, err := strconv.Atoi(os.Getenv("EXPIRES_IN_HOURS"))
+	if err != nil {
+		expiration = time.Hour * defaultExpiresInHours
+	} else {
+		expiration = time.Hour * time.Duration(expiresInHours)
+	}
+
+	token, err := GenerateJWT(jwtSecret, expiration)
 	if err != nil {
 		panic(err)
 	}
