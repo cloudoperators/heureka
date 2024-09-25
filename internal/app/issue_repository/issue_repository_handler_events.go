a0d5dd2180a651f7f7d2ea44e48b20a4f34be0c0
David Rochow <david.rochow@sap.com>
2024-09-25 13:05:48 +0200

    feat(nvd): allow fetching of issues from 2021 to now #247 (#250)
    * chore:  WIP for basic matching
    
    * chore: updated eventHandler initialization in tests
    
    * Change EventHandler signature and logic
    
    * Fix tests
    
    * Add more tests regarding concurrent processing
    
    * chore: refactored function for lower coginitive complexity & fixed logic
    
    * First implementation
    
    * Wip
    
    * Fix tests
    
    * Fixing tests part 2
    
    * Implement tests for BuildIssueVariantMap
    
    * Fix tests
    
    * Wip
    
    * chore(deps): bump github.com/prometheus/client_golang (#210)
    
    Bumps [github.com/prometheus/client_golang](https://github.com/prometheus/client_golang) from 1.20.2 to 1.20.3.
    - [Release notes](https://github.com/prometheus/client_golang/releases)
    - [Changelog](https://github.com/prometheus/client_golang/blob/v1.20.3/CHANGELOG.md)
    - [Commits](https://github.com/prometheus/client_golang/compare/v1.20.2...v1.20.3)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/prometheus/client_golang
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps): bump golang from 1.23.0 to 1.23.1 (#211)
    
    Bumps golang from 1.23.0 to 1.23.1.
    
    ---
    updated-dependencies:
    - dependency-name: golang
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Add tests for handling issue repositories with different priorities
    
    * chore: removed changes unrelated to this feature
    
    * fix: fixed imports
    
    * fix: fixed imports
    
    * Improve tests
    
    * resolve issues
    
    * feat(scanner/nvd): Adding possibility to fetch NVD from 2001
    
    * feat: made time window configurable
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Victor Dorneanu <victor.dorneanu@sap.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Michael Reimsbach <mr201190@gmail.com>

--- internal/app/issue_repository/issue_repository_handler_events.go
+++ internal/app/issue_repository/issue_repository_handler_events.go
@@ -8,7 +8,6 @@ import (
 	"github.com/cloudoperators/heureka/internal/app/event"
 	"github.com/cloudoperators/heureka/internal/database"
 	"github.com/cloudoperators/heureka/internal/entity"
-	"github.com/sirupsen/logrus"
 )
 
 const (
