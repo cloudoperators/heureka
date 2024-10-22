02bccecbe11b007d36784fd5141a9a1ce2475867
copyonwrite <73547592+copyonwrite@users.noreply.github.com>
2024-10-22 13:43:28 +0200

    Copyonwrite/248/add devcontainer setup (#286)
    * Initial support for devcontainers
    
    * Mound additional volume for heureka devcontainer
    
    * Add licence headers
    
    * Update README.md with devcontainer setup
    
    * Add environment test variables for devcontainer
    
    * Use devcontainer settings for tests in VSCode
    
    * Use string instead of boolean for SEED_MODE variable in docker-compose.yml
    
    * fix(test): added connection test for API Server
    
    * fix(test): use wait group for waiting for listening instead of periodic check
    
    * fix(test): removing AUTH_TYPE and AUTH_TOKEN_SECRET from compose yaml and set default AuthType to none
    
    * fix(test): added FirstListenThenServe to utility pkg and added in nonblocking server start and seperate token test
    
    ---------
    
    Co-authored-by: David Rochow <david.rochow@sap.com>
    Co-authored-by: Michael Reimsbach <MR201190@gmail.com>

---
+++ pkg/util/server.go
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package util
+
+import (
+	"net"
+	"net/http"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+)
+
+// FirstListenThenServe is a utility function that ensures that first a listener is spin up then the http server is setup for serving asynchronously
+// this is requried to ensure in tests that the server is spinned up before jumping to tests.
+func FirstListenThenServe(srv *http.Server) {
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(1)
+	go func() {
+		logrus.Info("Starting Non Blocking HTTP Server...")
+		ln, err := net.Listen("tcp", srv.Addr)
+		if err != nil {
+			logrus.WithError(err).Fatalf("Error while start listening...")
+		}
+		go func() {
+			waitGroup.Done()
+		}()
+		if err := srv.Serve(ln); err != nil && err != http.ErrServerClosed {
+			logrus.WithError(err).Fatalf("Error while serving HTTP Server.")
+		}
+	}()
+	waitGroup.Wait()
+}
