bde086728e5346da9890c30e0f94f6d35c9b75bb
David Rochow <david.rochow@sap.com>
2024-10-28 17:21:51 +0100

    fix(test): added additonal readiness checks for DB (#334)
    Co-authored-by: Michael Reimsbach <MR201190@gmail.com>

--- internal/e2e/component_instance_query_test.go
+++ internal/e2e/component_instance_query_test.go
@@ -6,13 +6,11 @@ package e2e_test
 import (
 	"context"
 	"fmt"
-	"os"
-	"time"
-
 	"github.com/cloudoperators/heureka/internal/entity"
 	testentity "github.com/cloudoperators/heureka/internal/entity/test"
 	"github.com/cloudoperators/heureka/internal/util"
 	util2 "github.com/cloudoperators/heureka/pkg/util"
+	"os"
 
 	"github.com/cloudoperators/heureka/internal/server"
 
@@ -32,8 +30,6 @@ var _ = Describe("Getting ComponentInstances via API", Label("e2e", "ComponentIn
 	var cfg util.Config
 
 	BeforeEach(func() {
-		// This sleep suppresses a potential racing condition which triggers test failures.
-		time.Sleep(3 * time.Second)
 
 		var err error
 		_ = dbm.NewTestSchema()
