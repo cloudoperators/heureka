2bd665766af37e8ad217698c9e7bf07d05012e29
Wolfram Fischer <wolfram.fischer@sap.com>
2024-12-19 11:54:03 +0100

    Add e2e test to create a ScannerRun

---
+++ internal/e2e/scanner_run_query_test.go
@@ -0,0 +1,91 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/cloudoperators/heureka/internal/util"
+	util2 "github.com/cloudoperators/heureka/pkg/util"
+
+	"github.com/cloudoperators/heureka/internal/server"
+
+	"github.com/cloudoperators/heureka/internal/api/graphql/graph/model"
+	"github.com/cloudoperators/heureka/internal/database/mariadb/test"
+	"github.com/machinebox/graphql"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+)
+
+var _ = Describe("Creating ScannerRun via API", Label("e2e", "ScannerRun"), func() {
+
+	var seeder *test.DatabaseSeeder
+	var s *server.Server
+	var cfg util.Config
+
+	BeforeEach(func() {
+		var err error
+		_ = dbm.NewTestSchema()
+		seeder, err = test.NewDatabaseSeeder(dbm.DbConfig())
+		Expect(err).To(BeNil(), "Database Seeder Setup should work")
+
+		cfg = dbm.DbConfig()
+		cfg.Port = util2.GetRandomFreePort()
+		s = server.NewServer(cfg)
+
+		s.NonBlockingStart()
+	})
+
+	AfterEach(func() {
+		s.BlockingStop()
+	})
+
+	When("the database has 10 entries", func() {
+
+		BeforeEach(func() {
+			seeder.SeedDbWithNFakeData(10)
+
+		})
+
+		Context("and a mutation query is performed", Label("create.graphql"), func() {
+			It("creates new service", func() {
+				sampleTag := gofakeit.Word()
+				sampleUUID := gofakeit.UUID()
+
+				// create a queryCollection (safe to share across requests)
+				client := graphql.NewClient(fmt.Sprintf("http://localhost:%s/query", cfg.Port))
+
+				//@todo may need to make this more fault proof?! What if the test is executed from the root dir? does it still work?
+				b, err := os.ReadFile("../api/graphql/graph/queryCollection/scannerRun/create.graphql")
+
+				Expect(err).To(BeNil())
+				str := string(b)
+				req := graphql.NewRequest(str)
+
+				req.Var("input", map[string]string{
+					"tag":  sampleTag,
+					"uuid": sampleUUID,
+				})
+
+				req.Header.Set("Cache-Control", "no-cache")
+				ctx := context.Background()
+
+				var respData struct {
+					ScannerRun model.ScannerRun `json:"createScannerRun"`
+				}
+				if err := util2.RequestWithBackoff(func() error { return client.Run(ctx, req, &respData) }); err != nil {
+					logrus.WithError(err).WithField("request", req).Fatalln("Error while unmarshaling")
+				}
+
+				Expect(*&respData.ScannerRun.ID).To(Equal(0))
+				Expect(*&respData.ScannerRun.Tag).To(Equal(sampleTag))
+				Expect(*&respData.ScannerRun.Tag).To(Equal(sampleUUID))
+			})
+		})
+	})
+})
