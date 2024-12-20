d02cff38a07499c432668aceee1256eb3197388d
Dustin Demmerle <dustindemmerle@gmail.com>
2024-12-20 15:14:45 +0000

    addid 2.2 check logic

--- scanner/openstack/modules/keystone/user_role_compliance.go
+++ scanner/openstack/modules/keystone/user_role_compliance.go
@@ -10,26 +10,68 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// policy2dot2Check checks if the given image name complies with policy 2.2.
+var adminCount int
+
+// policy2dot2Check checks if the given user complies with policy 2.2.
 // Policy 2.2 requires that project users do not contain the role "admin".
 //
 // Parameters:
-//
-//	roles ([]string): slice of roles for a given user
+// - userName (string): the name of the user
+// - roles ([]string): slice of roles for a given user
 //
 // Returns:
-//
-//	bool: Returns true if the image name complies with policy 2.2, otherwise false.
-func policy2dot2Check(roles []string) bool {
-	// This is a temporary hardcoded implementation of policy 2.2 for the OpenStack scanner PoC
-	// This function will be replaced by the actual implementation of policy checks in the future
-	// Policy 2.2 checks that the user roles contains
+// - bool: Returns true if the user complies with policy 2.2, otherwise false.
+func policy2dot2Check(userName string, roles []string) bool {
+	isAdmin := false
+	isTechnicalUser := strings.HasPrefix(userName, "TM3")
+	compliant := true
+
+	// Iterate over the roles to check for "registry_admin" and invalid roles for technical users
 	for _, role := range roles {
-		if role == "admin" {
+		if role == "registry_admin" {
+			isAdmin = true
+		}
+		if isTechnicalUser && strings.Contains(role, "admin") {
+			compliant = false
+			log.WithFields(log.Fields{
+				"user_name": userName,
+				"reason":    "Technical user cannot have roles containing 'admin'",
+			}).Info("User is non-compliant")
 			return false
 		}
 	}
-	return true
+
+	// Check if the user is both an admin and a technical user
+	if isAdmin && isTechnicalUser {
+		compliant = false
+		log.WithFields(log.Fields{
+			"user_name": userName,
+			"reason":    "Technical user cannot have 'registry_admin' role",
+		}).Info("User is non-compliant")
+		return false
+	}
+
+	// Check if the user is a technical user with admin roles
+	if isTechnicalUser && !isAdmin && strings.Contains(strings.Join(roles, ","), "admin") {
+		compliant = false
+		log.WithFields(log.Fields{
+			"user_name": userName,
+			"reason":    "Technical user with admin roles",
+		}).Info("User is non-compliant")
+		return false
+	}
+
+	// Check if the user is an admin with technical roles
+	if !isTechnicalUser && isAdmin && !strings.Contains(strings.Join(roles, ","), "admin") {
+		compliant = false
+		log.WithFields(log.Fields{
+			"user_name": userName,
+			"reason":    "Admin user with technical roles",
+		}).Info("User is non-compliant")
+		return false
+	}
+
+	return compliant
 }
 
 // ComputeUserRoleCompliance checks the compliance of the user roles in the OpenStack project.
@@ -75,7 +117,7 @@ func ComputeUserRoleCompliance(osScanner *scanner.Scanner, osProcessor *processo
 		fmt.Print("\n\n")
 	}
 
-	// Create component object for each server
+	// Create component object for each user
 	if len(users) != 0 {
 		for _, user := range users {
 			userName := user["user"].(string)
@@ -86,6 +128,7 @@ func ComputeUserRoleCompliance(osScanner *scanner.Scanner, osProcessor *processo
 				// Need to figure out how to handle this case in the future
 				log.Warning("user name is empty")
 				continue
+
 			}
 
 			// Join the slice into a single string
@@ -109,7 +152,7 @@ func ComputeUserRoleCompliance(osScanner *scanner.Scanner, osProcessor *processo
 			}
 
 			// Perform policy checks
-			if policy2dot2Check(userRoles) {
+			if policy2dot2Check(userName, userRoles) {
 				// Compliant
 				// Need to decide what to do here, if anything.
 				// but for now we can just log it
@@ -124,6 +167,31 @@ func ComputeUserRoleCompliance(osScanner *scanner.Scanner, osProcessor *processo
 				if err != nil {
 					log.WithError(err).Warning("Failed adding component version to issue")
 				}
+			}
+		}
+		// Check if the number of admin users is less than 2
+		if adminCount < 2 {
+			for _, user := range users {
+				userName := user["user"].(string)
+				userRoles := user["roles"].([]string)
+				joined := strings.Join(userRoles, ",")
+				hashedConf := osScanner.Md5Hash(joined)
+				componentId, err := processor.CreateComponentObject(*osProcessor, ctx, userName)
+				if err != nil {
+					log.WithError(err).Fatal("Error during createComponentObject")
+				}
+				componentVersionId, err := processor.CreateComponentVersionObject(*osProcessor, ctx, hashedConf, componentId)
+				if err != nil {
+					log.WithError(err).Fatal("Error during createComponentVersionObject")
+				}
+				err = osProcessor.ConnectComponentVersionToIssue(ctx, componentVersionId, issue22Id)
+				if err != nil {
+					log.WithError(err).Warning("Failed adding component version to issue")
+				}
+				log.WithFields(log.Fields{
+					"user_name": userName,
+					"reason":    "Less than 2 admin users",
+				}).Info("User is non-compliant")
 			}
 		}
 	}
