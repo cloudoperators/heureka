41dca6012c6ab69fa54950b61be82133830850ad
Michal Krzyz <michalkrzyz@gmail.com>
2025-09-12 10:26:16 +0200

    Run all Post Migration Procedures in parallel
    - Run all Post Migration Procedures in parallel
    and wait for finish.
    - Add e2e tests for post migration
    - Refactor e2e tests for migrations
    
    On-behalf-of: SAP Michal Krzyz <michal.krzyz@sap.com>
    Signed-off-by: Michal Krzyz <michalkrzyz@gmail.com>

---
+++ internal/e2e/migrations/99999999999999_add_post_migration.up.sql
@@ -0,0 +1,5 @@
+-- SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Greenhouse contributors
+-- SPDX-License-Identifier: Apache-2.0
+
+CALL add_post_migration_procedure('refresh_mvTestData_proc');
+
