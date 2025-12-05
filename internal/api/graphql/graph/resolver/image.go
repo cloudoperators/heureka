0e94d14a07a538f9e30da70c6829cdd838eac03c
Michael Reimsbach <mr201190@gmail.com>
2025-12-05 10:23:28 +0100

    fix(image): handle nil filter

--- internal/api/graphql/graph/resolver/image.go
+++ internal/api/graphql/graph/resolver/image.go
@@ -57,6 +57,9 @@ func (r *imageResolver) VulnerabilityCounts(ctx context.Context, obj *model.Imag
 func (r *imageResolver) Vulnerabilities(ctx context.Context, obj *model.Image, first *int, after *string, filter *model.VulnerabilityFilter) (*model.VulnerabilityConnection, error) {
 	rootCtx := baseResolver.GetRoot(graphql.GetFieldContext(ctx))
 	imageFilter := rootCtx.Args["filter"].(*model.ImageFilter)
+	if filter == nil {
+		filter = &model.VulnerabilityFilter{}
+	}
 	filter.Service = append(filter.Service, imageFilter.Service...)
 	return baseResolver.VulnerabilityBaseResolver(r.App, ctx, filter, first, after, &model.NodeParent{
 		Parent:     obj,
