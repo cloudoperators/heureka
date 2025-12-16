b39ef3e2efab2485f201f76d906f46e9b7772091
Michael Reimsbach <michael.reimsbach@sap.com>
2025-12-16 16:43:36 +0100

    fix(componentVersion): filter issueCounts by service ccrn (#1006)

--- internal/database/mariadb/migrations/20251031095712_mv_counters_replace_enum_rows_with_columns.up.sql
+++ internal/database/mariadb/migrations/20251031095712_mv_counters_replace_enum_rows_with_columns.up.sql
@@ -233,6 +233,7 @@ SET critical_count = CASE WHEN issue_value = 'Critical' THEN issue_count ELSE 0 
 
 ALTER TABLE mvCountIssueRatingsComponentVersion
 ADD COLUMN service_id INT DEFAULT NULL,
+ADD COLUMN service_ccrn VARCHAR(255) DEFAULT NULL,
 DROP COLUMN issue_value,
 DROP COLUMN issue_count,
 ADD COLUMN issue_count INT GENERATED ALWAYS AS (
@@ -246,6 +247,7 @@ BEGIN
     INSERT INTO mvCountIssueRatingsComponentVersion (
         component_version_id,
         service_id,
+        service_ccrn,
         critical_count,
         high_count,
         medium_count,
@@ -255,6 +257,7 @@ BEGIN
     SELECT
         CVI.componentversionissue_component_version_id AS component_version_id,
         CI.componentinstance_service_id AS service_id,
+        S.service_ccrn AS service_ccrn,
         COUNT(DISTINCT CASE WHEN IV.issuevariant_rating = 'Critical' THEN CONCAT(CVI.componentversionissue_component_version_id, ',', CVI.componentversionissue_issue_id) END) AS critical_count,
         COUNT(DISTINCT CASE WHEN IV.issuevariant_rating = 'High' THEN CONCAT(CVI.componentversionissue_component_version_id, ',', CVI.componentversionissue_issue_id) END) AS high_count,
         COUNT(DISTINCT CASE WHEN IV.issuevariant_rating = 'Medium' THEN CONCAT(CVI.componentversionissue_component_version_id, ',', CVI.componentversionissue_issue_id) END) AS medium_count,
@@ -263,6 +266,7 @@ BEGIN
     FROM ComponentVersionIssue CVI
     LEFT JOIN IssueVariant IV ON IV.issuevariant_issue_id = CVI.componentversionissue_issue_id
     INNER JOIN ComponentInstance CI ON CVI.componentversionissue_component_version_id = CI.componentinstance_component_version_id
+    INNER JOIN Service S ON CI.componentinstance_service_id = S.service_id
     LEFT JOIN Remediation R ON CI.componentinstance_service_id = R.remediation_service_id AND CVI.componentversionissue_issue_id = R.remediation_issue_id AND R.remediation_deleted_at IS NULL
     WHERE
     -- Count only non-remediated or with expired remediation
