b818df5a0bb27287e090c56b3b5cb2490ad27d23
kanstantsinbuklis-sap <kanstantsin.buklis@sap.com>
2026-08-12 09:37:31 +0200

    feat(siem): filter by name (#1281)
    Signed-off-by: Kanstantsin Buklis <kanstantsin.buklis@sap.com>

--- internal/api/graphql/graph/baseResolver/siem_alert.go
+++ internal/api/graphql/graph/baseResolver/siem_alert.go
@@ -38,6 +38,7 @@ func SIEMAlertBaseResolver(
 		IssueType:                []*string{&securityEvent},
 		IncludeComponentInstance: true,
 		IncludeService:           true,
+		PrimaryName:              filter.Name,
 		ServiceCCRN:              filter.Service,
 		SupportGroupCCRN:         filter.SupportGroup,
 		Region:                   filter.Region,
