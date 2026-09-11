21c1847c0202e6b4e9f68ef15903ddd15248b7e0
Valiantsin Tsimoshyk <v.tsimoshyk@sap.com>
2026-09-11 22:11:48 +0200

    test(escalation): added tests for escalation
    Signed-off-by: Valiantsin Tsimoshyk <v.tsimoshyk@sap.com>

--- internal/app/comment/comment_handler_test.go
+++ internal/app/comment/comment_handler_test.go
@@ -234,6 +234,7 @@ var _ = Describe("When creating a Comment", Label("app", "CreateComment"), func(
 
 			Expect(result).To(BeNil(), "no result should be returned")
 			Expect(err).ToNot(BeNil(), "error should be returned")
+
 			var appErr *appErrors.Error
 			Expect(errors.As(err, &appErr)).To(BeTrue(), "should be application error")
 			Expect(appErr.Code).To(Equal(appErrors.Internal), "should be Internal error")
